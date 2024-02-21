90820905c38b3f8904eee4ea22649eb95496e57c
Sri charan <charanbunny3925@gmail.com>
2024-02-21 16:57:51 +0530

    added analytics route

--- TASK-MANAGER-FRONTEND/src/components/DashboardComponents/rightSection/analyticsComponent/Analytics.jsx
+++ TASK-MANAGER-FRONTEND/src/components/DashboardComponents/rightSection/analyticsComponent/Analytics.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./analytics.module.scss"
 import Ellipse from "../../../../assets/icons/Ellipse.png"
 
 const Analytics = () => {
+  const [taskDetails, setTaskDetails] = useState({
+    backlogTasks: 0, todoTasks: 0, inProgressTasks: 0, completedTasks: 0
+  })
+
+  const [priorityDetails, setPriorityDetails] = useState({
+    lowPriority: 0, moderatePriority: 0, highPriority: 0
+  })
+
+  const [dueDateTasks, setDueDateTask] = useState(0)
+
   return (
     <main className={styles.main}>
       <p className={styles.text}>Analytics</p>
@@ -10,40 +20,54 @@ const Analytics = () => {
       <div className={styles.cardContainer}>
         <div className={styles.analyticsCard}>
           <div className={styles.fieldsContainer}>
-            <ul>
-              <li>Backlog Tasks</li>
-            </ul>
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>Backlog Tasks</p>
+            <p className={styles.fieldValue}>{taskDetails.backlogTasks}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>To-do Tasks</p>
+            <p className={styles.fieldValue}>{taskDetails.todoTasks}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>In-Progress Tasks</p>
+            <p className={styles.fieldValue}>{taskDetails.inProgressTasks}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>Completed Tasks</p>
+            <p className={styles.fieldValue}>{taskDetails.completedTasks}</p>
           </div>
         </div>
 
         <div className={styles.analyticsCard}>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>Low Priority</p>
+            <p className={styles.fieldValue}>{priorityDetails.lowPriority}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>Moderate Priority</p>
+            <p className={styles.fieldValue}>{priorityDetails.moderatePriority}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>High Priority</p>
+            <p className={styles.fieldValue}>{priorityDetails.highPriority}</p>
           </div>
 
           <div className={styles.fieldsContainer}>
-
+            <img src={Ellipse} alt="" className={styles.icon} />
+            <p className={styles.fieldName}>Due Date Tasks</p>
+            <p className={styles.fieldValue}>{dueDateTasks}</p>
           </div>
         </div>
       </div>
--- TASK-MANAGER-FRONTEND/src/components/DashboardComponents/rightSection/boardComponnet/Board.jsx
+++ TASK-MANAGER-FRONTEND/src/components/DashboardComponents/rightSection/boardComponnet/Board.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import styles from "./board.module.scss"
+import { format } from 'date-fns'
+import collapseIcon from "../../../../assets/icons/collapseIcon.png"
 
 const Board = () => {
+  const userName = localStorage.getItem("userName")
+  const currentDate = new Date();
+  const formattedDate = format(currentDate, 'do MMM, yyyy')
+
+
   return (
     <main className={styles.main}>
-      board
+      <header className={styles.header}>
+        <div className={styles.detailsContainer}>
+          <h1 className={styles.userName}>Welcome! {userName}</h1>
+          <p className={styles.text}>Board</p>
+        </div>
+
+        <div className={styles.dateAndFilterContainer}>
+          <p className={styles.date}>{formattedDate}</p>
+
+          <div className={styles.filter}>
+            <select name="" id="" className={styles.dropDown}>
+              <option value="today">Today</option>
+              <option value="thisWeek" selected>This week</option>
+              <option value="thisMonth">This Month</option>
+            </select>
+          </div>
+        </div>
+      </header>
+
+      <section className={styles.bottomSection}>
+        <div className={styles.taskContainer}>
+        <p>Backlog</p>
+        </div>
+
+        <div className={styles.taskContainer}>
+        <p>To do</p>
+
+        </div>
+
+        <div className={styles.taskContainer}>
+        <p>In progress</p>
+        </div>
+
+        <div className={styles.taskContainer}>
+        <p>Done</p>
+        </div>
+      </section>
     </main>
   )
 }
